530902bbe036377d98b91574f4350600c2e11e21
kholidabdrhmn <rahman.kurnia51@gmail.com>
2017-03-12 12:52:43 +0700

    tahapan awal

---
+++ src/ParkingLot/ParkingLot.java
@@ -0,0 +1,22 @@
+package ParkingLot;
+
+import java.util.Scanner;
+
+public class ParkingLot {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+
+	}
+	class Mobil{
+		String warna;
+		String nomor;
+		int slotNo;
+		
+		public Mobil(){
+			warna = "";
+			nomor = "";
+			slotNo = 0;
+		}
+	}
+}
