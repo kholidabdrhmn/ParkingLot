dd8e543495d1834b5647af596769ad321a6ee14d
kholidabdrhmn <rahman.kurnia51@gmail.com>
2017-03-12 14:23:27 +0700

    1
    Menghandle panjang slot parkir

--- src/ParkingLot/ParkingLot.java
+++ src/ParkingLot/ParkingLot.java
@@ -1,12 +1,27 @@
 package ParkingLot;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ParkingLot {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-
+		ArrayList<Mobil> lot;	
+		
+		while (sc.hasNextLine()) {
+			String a = sc.nextLine();
+			String[] input = a.split(" ");
+			
+			if (input[0].equalsIgnoreCase("create_parking_lot")) {
+				int slot = Integer.parseInt(input[1]);
+				lot = new ArrayList<>(slot);
+			}else if (input[0].equalsIgnoreCase("park")) {
+				
+			}
+			
+		}
+		sc.close();
 	}
 	class Mobil{
 		String warna;
