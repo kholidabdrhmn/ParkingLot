19a39eec5a4e53f2da483049ac32c67cbb7338d0
kholidabdrhmn <rahman.kurnia51@gmail.com>
2017-03-12 14:37:34 +0700

    1.1
    Selesai command set tempat parkir

--- src/ParkingLot/ParkingLot.java
+++ src/ParkingLot/ParkingLot.java
@@ -7,7 +7,7 @@ public class ParkingLot {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		ArrayList<Mobil> lot;	
+		Mobil[] lot;	
 		
 		while (sc.hasNextLine()) {
 			String a = sc.nextLine();
@@ -15,8 +15,20 @@ public class ParkingLot {
 			
 			if (input[0].equalsIgnoreCase("create_parking_lot")) {
 				int slot = Integer.parseInt(input[1]);
-				lot = new ArrayList<>(slot);
+				lot = new Mobil[slot];
+				System.out.println(lot.length);
+				System.out.println("Created a parking lot with "+slot+" slots");
 			}else if (input[0].equalsIgnoreCase("park")) {
+				
+			}else if (input[0].equalsIgnoreCase("leave")) {
+				
+			}else if (input[0].equalsIgnoreCase("registration_numbers_for_cars_with_colour")) {
+				
+			}else if (input[0].equalsIgnoreCase("slot_number_for_cars_with")) {
+				
+			}else if (input[0].equalsIgnoreCase("slot_number_for_registration_number")) {
+				
+			}else if (input[0].equalsIgnoreCase("status")) {
 				
 			}
 			
